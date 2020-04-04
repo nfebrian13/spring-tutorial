a1414cabf25d5acd56e559a067728ce7991bb0e6
nfebrian13 <febrian.nana@gmail.com>
2020-04-04 23:25:28 +0700

    commit

--- internet-banking/src/main/java/com/id/nana/boot/controller/NasabahController.java
+++ internet-banking/src/main/java/com/id/nana/boot/controller/NasabahController.java
@@ -1,14 +1,21 @@
 package com.id.nana.boot.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.id.nana.boot.dao.NasabahDao;
+import com.id.nana.boot.entity.Nasabah;
 
 @Controller
 @RequestMapping("/nasabah")
@@ -17,10 +24,10 @@ public class NasabahController {
 	@Autowired
 	private NasabahDao nasabahDao;
 
-	/* example : pageable
-	 * http://localhost:8080/nasabah/list?size=1&page=1
-	 * */
-	
+	/*
+	 * example : pageable http://localhost:8080/nasabah/list?size=1&page=1
+	 */
+
 	@GetMapping("/list")
 	public ModelMap tampilkanDataNasabah(Pageable page) {
 		ModelMap mm = new ModelMap();
@@ -29,11 +36,32 @@ public class NasabahController {
 	}
 
 	@GetMapping("/form")
-	public void tampilkanForm() {
+	public ModelMap tampilkanForm(@RequestParam(required = false, value = "id") Nasabah n) {
+		ModelMap mm = new ModelMap();
+
+		if (n == null) {
+			n = new Nasabah();
+		}
+
+		mm.addAttribute("nasabah", n);
+		return mm;
 	}
 
+	/*
+	 * ModelAttribute: adalah objek dari nasabah BindingResult : untuk menampung
+	 * error, dan membuat validasi SessionStatus : jika user telah input form dan
+	 * terjadi error, maka akan keep fieldnya di view
+	 */
+
 	@PostMapping("/form")
-	public String processForm() {
+	public String processForm(@ModelAttribute @Valid Nasabah n, BindingResult errors, SessionStatus status) {
+		if (errors.hasErrors()) {
+			return "nasabah/form";
+		}
+
+		nasabahDao.save(n);
+		status.setComplete();
+
 		return "redirect:list";
 	}
 }
