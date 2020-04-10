8751d625f77de13c6952d666d12035b77e8e1b51
nfebrian13 <febrian.nana@gmail.com>
2020-04-10 15:43:36 +0700

    commit

--- spring-trx-management/src/main/java/com/spring/trx/service/impl/OrganzationServiceImpl.java
+++ spring-trx-management/src/main/java/com/spring/trx/service/impl/OrganzationServiceImpl.java
@@ -2,6 +2,7 @@ package com.spring.trx.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.spring.trx.model.Employee;
 import com.spring.trx.model.EmployeeHealthInsurance;
@@ -18,13 +19,32 @@ public class OrganzationServiceImpl implements OrganizationService {
 	@Autowired
 	HealthInsuranceService healthInsuranceService;
 
+	/**
+	 * added @Transactional
+	 * Transaction is a cross cutting concern and it is implemented using AOP in Spring Boot.
+	 **/
+
 	@Override
+	@Transactional
 	public void joinOrganization(Employee employee, EmployeeHealthInsurance employeeHealthInsurance) {
 		employeeService.insertEmployee(employee);
+
+		/**
+		 * Skenario Test misalkan service untuk insert berhasil dan untuk pemanggilan
+		 * registerEmployeeHealthInsurance gagal karena error test skenario berikut
+		 * jika tidak menggunakan annotation @Transactional maka insertEmployee tetap diinsert
+		 * sedangkan harusnya keduanya tidak diinsert jika salah satu gagal atau ada error
+		 **/
+
+		if (employee.getEmpId().equals("emp1")) {
+			throw new RuntimeException("thowing exception to test transaction rollback");
+		}
+
 		healthInsuranceService.registerEmployeeHealthInsurance(employeeHealthInsurance);
 	}
 
 	@Override
+	@Transactional
 	public void leaveOrganization(Employee employee, EmployeeHealthInsurance employeeHealthInsurance) {
 		employeeService.deleteEmployeeById(employee.getEmpId());
 		healthInsuranceService.deleteEmployeeHealthInsuranceById(employeeHealthInsurance.getEmpId());
