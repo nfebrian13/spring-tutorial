74eb44db6c15b0a96fd6c8586dfc5906d7b367b3
nfebrian13 <febrian.nana@gmail.com>
2020-04-10 16:29:32 +0700

    initial commit

---
+++ springboot-trx-management-mybatis/src/main/java/com/trx/mybatis/SpringbootTrxManagementMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.trx.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTrxManagementMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTrxManagementMybatisApplication.class, args);
+	}
+
+}
